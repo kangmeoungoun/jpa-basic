a316e1536189ea1717fe2298432bf3539ad18ffd
kangmeoungoun <kes98202@naver.com>
2021-01-27 21:51:32 +0900

    프로젝션(SELECT)

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 ##### 객체지향 쿼리 언어 1 - 기본 문법
-##### 기본 문법과 쿼리 API
+##### 프로젝션(SELECT)
 
-![image](https://user-images.githubusercontent.com/40969203/105989892-14f04500-60e5-11eb-8e41-97a9f3c87827.png)
-![image](https://user-images.githubusercontent.com/40969203/105989906-17eb3580-60e5-11eb-8582-fe3e5b3d2cb4.png)
-![image](https://user-images.githubusercontent.com/40969203/105989915-1a4d8f80-60e5-11eb-831d-41bdecbf7db1.png)
-![image](https://user-images.githubusercontent.com/40969203/105989922-1cafe980-60e5-11eb-8257-34ba70efe3a9.png)
-![image](https://user-images.githubusercontent.com/40969203/105989931-1f124380-60e5-11eb-9484-489a45761916.png)
-![image](https://user-images.githubusercontent.com/40969203/105989937-21749d80-60e5-11eb-9542-ca5f6d14e48d.png)
-![image](https://user-images.githubusercontent.com/40969203/105989946-246f8e00-60e5-11eb-9523-bf0ea360f537.png)
-![image](https://user-images.githubusercontent.com/40969203/105989952-276a7e80-60e5-11eb-8ccc-7520730e83ce.png)
-![image](https://user-images.githubusercontent.com/40969203/105989976-2afe0580-60e5-11eb-84c0-10f6a547fc37.png)
+엔티티 프로젝션인 경우 영속성 컨텍스트에서 전부 관리가 된다. 몇건이든간에
+
+![image](https://user-images.githubusercontent.com/40969203/105993438-c2fdee00-60e9-11eb-83b5-2c1f27af8467.png)
+![image](https://user-images.githubusercontent.com/40969203/105993444-c6917500-60e9-11eb-87ee-a173f63d83bd.png)
--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -18,10 +18,20 @@ public class JpaMain{
             Member member =new Member();
             member.setUsername("member1");
             em.persist(member);
-            Member singleResult = em.createQuery("select m from Member m where m.username=:username" , Member.class)
-                                    .setParameter("username" , "member1")
-                                    .getSingleResult();
-            System.out.println("singleResult = " + singleResult.getUsername());
+            //엔티티 프로젝션
+            //List<Team> result = em.createQuery("select m.team from Member m join m.team t",Team.class)
+            //                .getResultList();
+            //임베디드 타입 프로젝션
+            //List<Address> result1 = em.createQuery("select o.address from Order o",Address.class)
+            //        .getResultList();
+
+            //스칼라 타입 프로젝션
+            //List<Object[]> resultList = em.createQuery("select distinct m.username,m.age from Member m").getResultList();
+            //Object[] objects = resultList.get(0);
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username,m.age) from Member m" , MemberDTO.class).getResultList();
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
             tx.commit();
         }catch (Exception e){
             tx.rollback();
