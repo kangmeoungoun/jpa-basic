273f856d2bb3e46cc228792a867cb9974a464d9a
kangmeoungoun <kes98202@naver.com>
2021-01-29 16:15:41 +0900

    경로 표현식

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-##### 객체지향 쿼리 언어 1 - 기본 문법
-##### JPQL 함수
+##### 객체지향 쿼리 언어 2 - 중급 문법
+##### 경로 표현식
 
-![image](https://user-images.githubusercontent.com/40969203/106240176-29e4e980-6247-11eb-90bc-fd317ac99644.png)
-![image](https://user-images.githubusercontent.com/40969203/106240185-2cdfda00-6247-11eb-8e12-13ebac99ceb2.png)
+![image](https://user-images.githubusercontent.com/40969203/106243604-0886fc00-624d-11eb-9b66-f130a75deeda.png)
+![image](https://user-images.githubusercontent.com/40969203/106243608-0c1a8300-624d-11eb-8c5c-4ff0cb0d023d.png)
+![image](https://user-images.githubusercontent.com/40969203/106243620-1177cd80-624d-11eb-89bf-048445f4598a.png)
+![image](https://user-images.githubusercontent.com/40969203/106243638-150b5480-624d-11eb-81d0-2005166347f4.png)
+![image](https://user-images.githubusercontent.com/40969203/106243650-18064500-624d-11eb-9e46-200c759f6fb0.png)
+![image](https://user-images.githubusercontent.com/40969203/106243669-1b013580-624d-11eb-9cfc-375cf235e9e1.png)
+![image](https://user-images.githubusercontent.com/40969203/106243681-1e94bc80-624d-11eb-8c31-7efeed19e841.png)
+![image](https://user-images.githubusercontent.com/40969203/106243693-22284380-624d-11eb-8d66-a0cb628f941a.png)
+![image](https://user-images.githubusercontent.com/40969203/106243702-25233400-624d-11eb-8b4c-7cf642373407.png)
+
--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain{
@@ -15,29 +16,24 @@ public class JpaMain{
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-                Member member1 = new Member();
-                member1.setUsername("관리자1");
-                em.persist(member1);
-                Member member2 = new Member();
-                member2.setUsername("관리자2");
-                em.persist(member2);
-                em.flush();
-                em.clear();
-                /*String sql = "select " +
-                            " case when m.age <=10 then '학생요금'" +
-                            "      when m.age >=60 then '경로요금'" +
-                            "      else '일반요금' end as str" +
-                            " from Member m";
-
-
-                String sql = "select coalesce(m.username,'이름 없는 회원') as username from Member m";
-                */
-
-                String sql = "select group_concat(m.username) as username from Member m";
+            Team team = new Team();
+            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            member1.setTeam(team);
+            em.persist(member1);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setTeam(team);
+            em.persist(member2);
+            em.flush();
+            em.clear();
+            String sql = "select m.username from Team t join t.members m";
             List<String> resultList = em.createQuery(sql , String.class).getResultList();
             for (String s : resultList) {
                 System.out.println("s = " + s);
             }
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
